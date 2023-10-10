ce279b8e2949b8f2b9896b03526873100f7436cb
Ty Jackson <143833425+TjacksWebDev@users.noreply.github.com>
2023-10-10 08:45:20 -0400

    updating super user and normal user

--- src/com/coderscampus/base/NormalUser.java
+++ src/com/coderscampus/base/NormalUser.java
@@ -2,4 +2,10 @@ package com.coderscampus.base;
 
 public class NormalUser extends User {
 
+    public NormalUser (String username, String password, String name) {
+		this.role = "normal_user";
+		this.setUsername(username);
+		this.setPassword(password);
+		this.setName(name);
+	}
 }
